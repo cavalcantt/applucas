3344f67e06d5ab0bd0191430816b03caa21fb8bf
cavalcantt <ruansilvacavalcanti@gmail.com>
2022-06-05 17:07:14 -0300

    Usando hooks no exercicios  React.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect,useMemo,useCallback} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App (){
+
+  const [tarefas, setTarefas] = useState([
+    'pagar a conta de luz', 'Estudar React Hooks']);
+
+
+    useEffect(()=>{
+     const tarefasStorage = localStorage.getItem('tarefas');
+
+     if(tarefasStorage){
+       setTarefas(JSON.parse(tarefasStorage));
+     }
+   },[]); 
+
+    useEffect(()=>{localStorage.setItem('tarefas',JSON.stringify(tarefas));},[tarefas])
+    
+  const [input, setinput]= useState ('');
+
+  
+  const handleAdd= useCallback(()=> {
+     setTarefas([...tarefas,input])
+     setinput ('')
+   },[input,tarefas]);
+
+  const totalTarefas= useMemo(() => tarefas.length,[tarefas]);
+  
+    return(
+
+    <div>
+
+       <ul> {tarefas.map(tarefa =>(<li key={tarefa}>{tarefa}</li>))}
+       </ul>
+        <br/>
+      <strong>Voce tem {totalTarefas} tarefas!</strong> 
+       <br/>
+        <input type="text" value={input}onChange={e=>setinput(e.target.value)}></input>
+        <button type="button" onClick = {handleAdd}>Adicionar</button>
+        
+      </div>
   );
 }
 
